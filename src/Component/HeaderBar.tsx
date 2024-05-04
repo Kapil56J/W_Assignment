9f564b6ef3fe77f5fa3d16b00e62fd7b2c59e7d4
Kapil56J <kapiljadhav5656@gmail.com>
2024-05-04 16:23:09 +0530

    init

---
+++ src/Component/HeaderBar.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Typography } from "@mui/material";
+
+const HeaderBar = () => {
+
+    return (
+        <div>
+            <Typography variant="h4">Hello React JS</Typography>
+        </div>
+    );
+};
+
+export default HeaderBar;
